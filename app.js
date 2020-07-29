0e5bc2e38d8c359c4fdfb9074f043eefcf3bb007
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-07-29 09:07:29 +0200

    Add script to generate ID

---
+++ app.js
@@ -0,0 +1,47 @@
+const os = require('os');
+const fs = require('fs');
+
+
+const gender = ['M', 'F'];
+const maleFirstName = ['John', 'Bruce', 'Shane', 'Archie', 'Gary', 'Charles'];
+const femaleFirstName = ['Hannah', 'Courtney', 'Emily ', 'Nadia', 'Maddie', 'Connie'];
+const lastName = ['Graham', 'King', 'McCann', 'Parker', 'Wright', 'Reid'];
+
+const people = [];
+
+const randChoice = (data) => {
+    const index = Math.floor(Math.random() * data.length);
+    return data[index];
+}
+
+
+for(let i = 0; i <= 20; i++) {
+    const randGender = randChoice(gender);
+    let randFirstName = '';
+    const randLastName = randChoice(lastName);
+    const randAge = Math.floor(Math.random() * (78 - 18 + 1) + 18);
+
+    if(randGender === 'M') {
+        randFirstName = randChoice(maleFirstName)
+    } else {
+        randFirstName = randChoice(femaleFirstName)
+    };
+
+    const randEmail = `${randFirstName}.${randLastName}@gmail.com`
+
+
+    const personalData = {
+        gender: randGender,
+        firstName: randFirstName,
+        lastName: randLastName,
+        age: randAge,
+        email: randEmail.toLowerCase(),
+    }
+
+    people.push(personalData);
+}
+
+fs.writeFile('data.json', JSON.stringify(people), 'utf8', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
